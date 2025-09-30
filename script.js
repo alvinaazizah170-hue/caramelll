383ddb3d5373338f89e4d3cc0959c218c9f92c5d
alvinaazizah170-hue <alvinaazizah170@gmail.com>
2025-09-30 14:13:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// 1. Pilih tombol berdasarkan ID-nya
+conts tombolSapa = document.getElementByld('sapautton');
+// 2. Tambah 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click',function() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
